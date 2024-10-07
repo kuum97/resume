bf69d53393801d73b33ad04f17842c33f7626693
kuum97 <kuum97@icloud.com>
2024-10-07 19:57:39 +0900

    Add pretendard font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,17 @@
 import type { Metadata } from "next";
+import localFont from "next/font/local";
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Resume",
   description: "Hi! My name is JuHyun Kwon. Introduce myself.",
 };
+
+const pretendard = localFont({
+  src: "./fonts/PretendardVariable.woff2",
+  display: "swap",
+  weight: "45 920",
+});
 
 export default function RootLayout({
   children,
@@ -13,7 +20,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body
+        className={`${pretendard.className} flex flex-col justify-center items-center w-screen`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
