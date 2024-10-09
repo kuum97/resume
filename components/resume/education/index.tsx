5695a15c47840ffd98a104f34c6e90c031d1bd5a
kuum97 <kuum97@icloud.com>
2024-10-09 17:40:30 +0900

    Feat education component

---
+++ components/resume/education/index.tsx
@@ -0,0 +1,19 @@
+export default function Education() {
+  return (
+    <div>
+      <h3 className="mb-3">교육</h3>
+      <ul className="pl-4 font-medium list-disc">
+        <li>
+          <p className="text-sm font-medium">코드잇 프론트엔드 트랙 5기 수료</p>
+          <p className="text-sm font-light text-slate-600">
+            2024.02.15 ~ 2024.08.12
+          </p>
+        </li>
+        <li>
+          <p className="text-sm font-medium">용호고등학교 졸업</p>
+          <p className="text-sm font-light text-slate-600">2013.03 ~ 2016.02</p>
+        </li>
+      </ul>
+    </div>
+  );
+}
