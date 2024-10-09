7f3eeff01bb2a97dcfbdf5254a1e881edb0408f3
kuum97 <kuum97@icloud.com>
2024-10-09 11:42:05 +0900

    Fix tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,15 +2,16 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        black: "#1B1B1B",
+      },
+      aspectRatio: {
+        a4: "1 / 1.414",
       },
     },
   },
